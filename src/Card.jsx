4e2f4298ffb4dcfbe8af203b6801136b00d928a4
Mariia <whitesheepa@gmail.com>
2021-11-29 05:27:25 +0200

    card

--- src/Card.jsx
+++ src/Card.jsx
@@ -6,17 +6,16 @@ const Card = ({ user }) => {
   return (
     <div className="userCard">
       <li className="userListItem" key={uuidv4()}>
-        <h5>{user.name}</h5>
-        <br />
         <img
           src={`https://eu.ui-avatars.com/api/?name=${user.name}`}
           alt={user.name}
         />
         <br />
+        <h5>{user.name}</h5>
         <i>
           <p>@{user.username}</p>
         </i>
-        <a href={user.website}>{user.website}</a>
+        <a href={`https://${user.website}`}>{`https://${user.website}`}</a>
         <br />
         <Link to={`/${user.id}`}>
           <button className="moreButton btn btn-primary raise">
